d896b1df5cfe89b898cdc4bb508242c78febf689
judetsuki <luka9609@gmail.com>
2026-02-13 11:38:40 +0300

    added more models

--- product_test/product_app/models.py
+++ product_test/product_app/models.py
@@ -5,7 +5,27 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
     stock = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    discount = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.CharField()
+
+
+class User(models.Model):
+    role = models.CharField()
+    user_name = models.CharField()
+    password = models.CharField()
+    login = models.CharField()
+
+class Order(models.Model):
+    order_date = models.DateField()
+    delivery_date = models.DateField()
+    status = models.CharField()
+
+class orderDetails(models.Model):
+    order_id = models.ForeignKey(Order, on_delete = models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete = models.CASCADE)
+    order_quantity = models
+    
